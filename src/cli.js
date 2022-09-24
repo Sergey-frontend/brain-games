73dba9c36a412ccae224edd9cd152706ba079ab4
Сергей Толстиков <tolstikovsn@gmail.com>
2022-09-24 23:04:37 +0500

    3.added dir 'src' and file 'cli.js'

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+const howYourName = () => {
+    const name = readlineSync.question('May I have your name?: ');
+    return `Hello, ${name}!`;
+}
+export {howYourName};

c240dac26842886092335dbe732b1be9aa9a923c
Сергей Толстиков <tolstikovsn@gmail.com>
2022-10-23 00:08:17 +0600

    update file brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,25 +7,29 @@ console.log(`Hello, ${name}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
-let randomNumber = Math.round(Math.random() * 100);
 
-const userAnswer = readlineSync.question(`Question: ${randomNumber} `);
+for (let i = 0; i < 3;i += 1) {
+  
+  let randomNumber = Math.round(Math.random() * 100);
+  const userAnswer = readlineSync.question(`Question: ${randomNumber} `);
 
-const isEven = randomNumber => randomNumber % 2 === 0;
-const expectedAnswer = isEven(randomNumber) ? 'yes' : 'no';
-if (expectedAnswer === userAnswer) {
-  console.log('Correct!');
+  const isEven = randomNumber => randomNumber % 2 === 0;
+  const expectedAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+  const isNotEven = randomNumber => randomNumber % 2 !== 0;
+  const wrongAnswer = isNotEven(randomNumber) ? 'yes' : 'no';
+
+  if (expectedAnswer === userAnswer) {
+    console.log('Correct!');
+  } else if (wrongAnswer === userAnswer) {
+    console.log(`'${wrongAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
+    break;
+  } else if (userAnswer !== 'yes' && userAnswer !== 'no') {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
+    break;
+      } 
 }
 
-const isNotEven = randomNumber => randomNumber % 2 !== 0;
-const wrongAnswer = isNotEven(randomNumber) ? 'yes' : 'no';
-if (wrongAnswer === userAnswer) {
-  console.log(`'${wrongAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
-}
-
-const thisIsNotAnAnswer = () => {
-    if (userAnswer !== 'yes' && userAnswer !== 'no') {
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
-    }
-}
-thisIsNotAnAnswer();
+// if (i === 2) {
+//   console.log('helllllllllo')
+// }

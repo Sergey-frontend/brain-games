7578285cf49f16a648a35b6acaebb5d8b787765d
Сергей Толстиков <tolstikovsn@gmail.com>
2022-10-23 19:52:22 +0600

    add brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,31 @@
+// #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('May I have your name?: ');
+console.log(`Hello, ${name}!`);
+
+console.log('What is the result of the expression?');
+
+
+
+
+for (let i = 0; i < 3; i += 1) {
+    let randomNumber1 = Math.round(Math.random() * 200);
+    let randomNumber2 = Math.round(Math.random() * 200);
+
+    const operands = ['+','-','*'];
+    const operand = Math.floor(Math.random() * operands.length);
+
+    switch (operands[operand]) {
+        case '+':
+            const userAnswer = readlineSync.question(`Question: ${randomNumber1} + ${randomNumber2} `);
+            const userResult = userAnswer;
+            const result = randomNumber1 + randomNumber2;
+            if (userResult === result) {
+                console.log('Correct!');
+            } else if (userResult !== result) {
+                console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again,${name}!`);
+            }
+            break;
+        }
+    }

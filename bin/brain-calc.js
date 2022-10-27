916933464a09a47fb0d84d031dd5882a0f6f220b
Сергей Толстиков <tolstikovsn@gmail.com>
2022-10-27 20:59:04 +0600

    update brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -19,12 +19,35 @@ for (let i = 0; i < 3; i += 1) {
     switch (operands[operand]) {
         case '+':
             const userAnswer = readlineSync.question(`Question: ${randomNumber1} + ${randomNumber2} `);
-            const userResult = userAnswer;
+            const userResult = Number(userAnswer);
             const result = randomNumber1 + randomNumber2;
             if (userResult === result) {
                 console.log('Correct!');
             } else {
                 console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again,${name}!`);
+                break;
+            }
+            break;
+        case '-':
+            const userAnswer2 = readlineSync.question(`Question: ${randomNumber1} - ${randomNumber2} `);
+            const userResult2 = Number(userAnswer2);
+            const result2 = randomNumber1 - randomNumber2;
+            if (userResult2 === result2) {
+                console.log('Correct!');
+            } else {
+                console.log(`'${userAnswer2}' is wrong answer ;(. Correct answer was '${result2}'.\nLet's try again,${name}!`);
+                break;
+            }
+            break;
+        case '*':
+            const userAnswer3 = readlineSync.question(`Question: ${randomNumber1} * ${randomNumber2} `);
+            const userResult3 = Number(userAnswer3);
+            const result3 = randomNumber1 * randomNumber2;
+            if (userResult3 === result3) {
+                console.log('Correct!');
+            } else {
+                console.log(`'${userAnswer3}' is wrong answer ;(. Correct answer was '${result3}'.\nLet's try again,${name}!`);
+                break;
             }
             break;
         }

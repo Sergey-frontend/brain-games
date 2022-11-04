92052f153711e7767fbc86f8c9fd9f30735da55a
Сергей Толстиков <tolstikovsn@gmail.com>
2022-11-04 14:24:48 +0600

    spelling error fixed

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const howYourName = () => {
-    const name = readlineSync.question('May I have your name?: ');
+    const name = readlineSync.question('May I have your name? ');
     return `Hello, ${name}!`;
 }
 export {howYourName};

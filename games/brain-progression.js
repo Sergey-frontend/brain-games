34087b6b296370db8cc7bdbe34421a9dc099c3da
Сергей Толстиков <tolstikovsn@gmail.com>
2022-11-04 18:04:37 +0600

    fixed brain-prog2

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -7,16 +7,16 @@ export const brainProg = () => {
   for (let i = 0; i < 3; i += 1) {
     let startProgNamber = Math.round(Math.random() * 50);
     const randomProgValue = Math.floor(Math.random() * (10 - 2 + 1)) + 2;
-    const randomProgLength = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+    const randomProgLength = 10;
     let progressionArr = [startProgNamber];
     for (let i = 1; i < randomProgLength; i += 1) {
       startProgNamber = startProgNamber + randomProgValue;
       progressionArr.push(startProgNamber);
     }
-    const middleSymbol = Math.round((progressionArr.length - 1) / 2);
+    const middleSymbol = 5;
     const correctAnswer = Number(progressionArr[middleSymbol]);
     progressionArr[middleSymbol] = "..";
-    const progressionString = progressionArr.join();
+    const progressionString = progressionArr.join(' ');
     const userAnswer = readlineSync.question(`Question: ${progressionString}\nYour answer: `);
 
     if (Number(userAnswer) === correctAnswer) {

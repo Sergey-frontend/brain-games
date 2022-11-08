190ea884b75651231d810d7108445d64c4918799
Сергей Толстиков <tolstikovsn@gmail.com>
2022-11-08 23:04:59 +0600

    7, update brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,34 +1,26 @@
-import readlineSync from 'readline-sync';
-import name from '../index.js';
+import { getRandomNumber } from '../helper.js';
+import runGame from '../index.js';
+
 const description = 'Find the greatest common divisor of given numbers.';
-const brainGcd = () => {
-  console.log(description);
 
-  for (let i = 0; i < 3; i += 1) {
-    let randomNumber1 = Math.round(Math.random() * 50);
-    let randomNumber2 = Math.round(Math.random() * 50);
-
-    const userAnswer = readlineSync.question(`Question: ${randomNumber1} ${randomNumber2}\nYour answer: `);
-    const typeNumberOfUserAnswer = Number(userAnswer);
-
-    while (randomNumber1 !== randomNumber2) {
-      if (randomNumber1 > randomNumber2) {
-        randomNumber1 -= randomNumber2;
-      } else {
-        randomNumber2 -= randomNumber1;
-      }
-    }
-    const gcd = randomNumber1;
-    if (typeNumberOfUserAnswer === gcd) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gcd}'.\nLet's try again, ${name}!`);
-      break;
-    }
-    if (i === 2) {
-      console.log(`Congratulations, ${name}!`);
-    }
+const nod = (x, y) => {
+  if (y > x) {
+    return nod(y, x);
+  } if (!y) {
+    return x;
   }
+  return nod(y, x % y);
+};
+
+const getQuestionAndAnswer = () => {
+  const firstNumber = getRandomNumber(0, 50);
+  const secondNumber = getRandomNumber(0, 50);
+  const question = `${firstNumber} ${secondNumber}`;
+  const correctAnswer = String(nod(firstNumber, secondNumber));
+  return [question, correctAnswer];
+};
+const brainGcd = () => {
+  runGame(description, getQuestionAndAnswer);
 };
 
 export default brainGcd;

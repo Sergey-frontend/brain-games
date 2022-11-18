816f927e6cf14639e13307e2e94cc487bdb61c34
Сергей Толстиков <tolstikovsn@gmail.com>
2022-11-18 14:51:59 +0600

    fix brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,15 +5,15 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 const isEven = (num) => {
   if (num % 2 === 0) {
-    return 'yes';
+    return true;
   }
-  return 'no';
+  return false;
 };
 
 const getQuestionAndAnswer = () => {
   const randomNumber = getRandomNumber(0, 50);
   const question = `${randomNumber}`;
-  const correctAnswer = isEven(randomNumber);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 

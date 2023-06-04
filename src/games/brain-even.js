73e76259e53c02e27983ba0c632943289cc95cf5
SergeyT <tolstikovsn@gmail.com>
2023-06-04 19:46:22 +0500

    refactoring brain-even/prime

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,12 +5,14 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 const isEven = (num) => num % 2 === 0;
 
-const generateEvenQuestionAndAnswer = () => {
-  const randomNumber = getRandomNumber(0, 50);
+const generateQuestionAndAnswer = (min, max) => {
+  const randomNumber = getRandomNumber(min, max);
   const question = `${randomNumber}`;
   const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
+
+const generateEvenQuestionAndAnswer = () => generateQuestionAndAnswer(0, 50);
 
 const brainEven = () => {
   runGame(description, generateEvenQuestionAndAnswer);
--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,21 +1,26 @@
 import getRandomNumber from '../helper.js';
 import runGame from '../index.js';
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const description = 'Answer "yes" if the given number is prime. Otherwise, answer "no".';
 
-const isPrime = (x) => {
-  for (let y = 2; y < x; y += 1) {
-    if (x % y === 0) {
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let divisor = 2; divisor <= Math.sqrt(number); divisor += 1) {
+    if (number % divisor === 0) {
       return false;
     }
   }
+
   return true;
 };
 
 const generatePrimeQuestionAndAnswer = () => {
-  const firstNumber = getRandomNumber(3, 15);
-  const question = `${firstNumber}`;
-  const correctAnswer = isPrime(firstNumber) ? 'yes' : 'no';
+  const number = getRandomNumber(3, 15);
+  const question = `${number}`;
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
